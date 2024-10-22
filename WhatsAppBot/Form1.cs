db7b0dc0a2806cf9ac2bbce0e47afc1bd680a309
DevIctorC <jvictor.devprofissional@gmail.com>
2024-10-22 18:48:18 -0300

    Adicionar arquivos de projeto.

---
+++ WhatsAppBot/Form1.cs
@@ -0,0 +1,16 @@
+namespace WhatsAppBot
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WhatsAppSendMessage w = new WhatsAppSendMessage();
+            w.SendMessage("Caloteiraaaa, Paga o dia logo!", "Nega/Dengo");
+        }
+    }
+}
---
+++ WhatsAppBot/WhatsAppSendMessage.cs
@@ -0,0 +1,36 @@
+﻿using EasyAutomationFramework;
+using org.junit.experimental.theories.@internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatsAppBot
+{
+    public class WhatsAppSendMessage : Web
+    {
+        public void SendMessage(string message, string to)
+        {
+            try
+            {
+                StartBrowser(TypeDriver.GoogleChorme, "C:\\Users\\jaovi\\AppData\\Local\\Google\\Chrome\\User Data");
+
+                Navigate("https://web.whatsapp.com/");
+
+                WaitForLoad();
+
+                var elementSearch = AssignValue(TypeElement.Xpath, "//*[@id=\"side\"]/div[1]/div/div[2]/div[2]/div/div/p", to);
+                elementSearch.element.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+                var elementMessage = AssignValue(TypeElement.Xpath, "//*[@id=\"main\"]/footer/div[1]/div/span/div/div[2]/div[1]/div/div[1]/p", message);
+                elementMessage.element.SendKeys(OpenQA.Selenium.Keys.Enter);
+            }
+
+            catch {
+                Console.WriteLine("ERRO DE EXECUÇÃO");
+            }
+
+        }
+    }
+}
